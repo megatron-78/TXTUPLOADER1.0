56f5d23f7f17750c1536ea439a494c5a1d7f373e
Shan9413 <159189394+Shan9413@users.noreply.github.com>
2024-02-28 00:21:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = API_ID = 20088962
+API_ID = API_ID = 28328736
 
-API_HASH = os.environ.get("API_HASH", "257f47d347157555890a64b12bc0134f")
+API_HASH = os.environ.get("API_HASH", "802254a44896baa87f3083b7af36b2e5")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6248136008:AAFD70s4VVn04Rb6bg3fTuLFC9hdeyxrOEk")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6901520266:AAGBgIDCdzCXSmD729Pk7tXojjvEr4qZJj8")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
