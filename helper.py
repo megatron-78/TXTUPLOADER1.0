c48168e2d053c68b879068dbde40bd2682080cf6
Virendraverma9950 <153084063+Virendraverma9950@users.noreply.github.com>
2024-04-01 21:48:08 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -108,12 +108,12 @@ async def download_video(url, cmd, name):
 
 
 
-async def send_vid(bot: Client, m: Message, cc, filename, thumb, name,):
+async def send_vid(bot: Client, m: Message, cc, filename, thumb, name, prog):
 
     subprocess.run(
         f'ffmpeg -i "{filename}" -ss 00:00:12 -vframes 1 "{filename}.jpg"',
         shell=True)
-    #await prog.delete(True)
+        await prog.delete(True)
     reply = await m.reply_text(f"**Uploading ...** - `{name}`")
     try:
         if thumb == "no":
