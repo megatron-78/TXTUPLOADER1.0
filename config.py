13baf3c3e319ccd1666cbb2b6ec6db77e26054a1
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-04-24 16:36:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = API_ID = 23291931
 
-API_HASH = os.environ.get("API_HASH", "4b11dd648188731fb7c9bc8083e8791c)
+API_HASH = os.environ.get("API_HASH", "4b11dd648188731fb7c9bc8083e8791c")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7158816113:AAF_JswD0TKiQ2otbDSs9jHs9qZ913a236Y")
 
