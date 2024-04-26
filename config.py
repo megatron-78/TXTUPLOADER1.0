156536d0cef20fede6d1182cdd5776ae4c64bfff
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-04-26 14:15:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = API_ID = "23291931"
 
 API_HASH = os.environ.get("API_HASH", "4b11dd648188731fb7c9bc8083e8791c")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7158816113:AAF_JswD0TKiQ2otbDSs9jHs9qZ913a236Y")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6946359667:AAEUE6wpdPmY5qCHdzvedwzl015Vkz9sYVM")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
